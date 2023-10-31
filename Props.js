50834db4dd1e5af2cd278e26ce850f3798d9d874
Thanu824 <142902810+Thanu824@users.noreply.github.com>
2023-11-01 00:22:22 +0530

    Update Props.js

--- Props.js
+++ Props.js
@@ -15,21 +15,3 @@ const Props = (Props) => {
     </>
   );
 };
-export default Props;
-
-import React from 'react'
-import '../Components/Poststyle.css'
-import profile from '../images/image1.jpg'
-
-function Post() {
-  return (
-    <div className='post'>
-        <img className='image' src={profile}/>
-        <h2 style={{color:'blue'}}>12451</h2>
-        <h2>12</h2>
-        <h2 >Rs.110.00000|8756345389303835708</h2>
-    </div>
-  )
-}
-
-export default Post*/
